0e91ef3ebcdbfe1306ea76f845534a3711e6d5fc
Sushil <sushilkumar96176@gmail.com>
2021-03-06 21:05:17 +0530

    Spring Initializer

---
+++ src/test/java/com/freshvotes/FreshVotes/FreshVotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freshvotes.FreshVotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshVotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
